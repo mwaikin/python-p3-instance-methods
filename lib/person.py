eadb0c5bdf9b0266046ac2dc3f782ff5eb4c265a
mwaikin <mwaikingori10@gmail.com>
2024-05-22 11:47:05 +0300

    passed

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
+    def talk(self):
+        print("Hello World!")
 
-    #Instance method definition
-    pass
+    def walk(self):
+        print("The person is walking.")
+
+
+
+person1 = Person()
+person1.talk()
+
+person2 = Person()
+person2.walk()
